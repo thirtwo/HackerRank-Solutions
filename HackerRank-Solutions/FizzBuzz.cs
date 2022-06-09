451a67570df174450e31e27b61aadbe9d3824790
thirtwo <65511956+thirtwo@users.noreply.github.com>
2022-06-09 19:33:25 +0300

    add Fizz Buzz

---
+++ HackerRank-Solutions/FizzBuzz.cs
@@ -0,0 +1,59 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+class Result
+{
+
+    /*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(GetFizzBuzz(i));
+        }
+    }
+    private static string GetFizzBuzz(int n)
+    {
+        string s = "";
+        if (n % 3 == 0)
+        {
+            s += "Fizz";
+        }
+        if (n % 5 == 0)
+        {
+            s += "Buzz";
+        }
+        if (string.IsNullOrEmpty(s))
+        {
+            s = n.ToString();
+        }
+        return s;
+    }
+
+}
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.fizzBuzz(n);
+    }
+}
