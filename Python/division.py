50343e019219bb4bd49421f49b56717fb1a2deb9
thirtwo <thirtwo@protonmail.com>
2025-03-01 14:35:11 +0100

    Create division.py

---
+++ Python/division.py
@@ -0,0 +1,8 @@
+def DivideAndPrint(number_a,number_b):
+    print(number_a//number_b)
+    print(number_a/number_b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    DivideAndPrint(a,b)
